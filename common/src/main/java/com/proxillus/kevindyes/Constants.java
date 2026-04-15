df7b31077273b2e80af787f22ed024294b8038e2
Kevin <couragedog30@yahoo.com>
2026-04-15 13:04:06 -0500

    Initial commit

---
+++ common/src/main/java/com/proxillus/kevindyes/Constants.java
@@ -0,0 +1,11 @@
+package com.proxillus.kevindyes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "kevindyes";
+	public static final String MOD_NAME = "Kevin Dyes";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/com/proxillus/kevindyes/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.proxillus.kevindyes.platform;
+
+import com.proxillus.kevindyes.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
